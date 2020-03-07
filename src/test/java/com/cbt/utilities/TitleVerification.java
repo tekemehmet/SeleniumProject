56930fc627e9966b7b83d8c07aa46b0141f5133d
tekemehmet <tekemehmet@yahoo.com>
2020-03-06 23:11:14 -0500

    Update-1

---
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -0,0 +1,56 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.*;
+
+public class TitleVerification {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.chromedriver().version("79.0").setup();
+        WebDriver driver = new ChromeDriver();
+
+        List<String> urls = Arrays.asList("http://practice.cybertekschool.com/",
+                "http://practice.cybertekschool.com/dropdown",
+                "http://practice.cybertekschool.com/login");
+
+        Set<String> titles = new HashSet<>();
+        List<String> currentURLs = new ArrayList<>();
+
+        for (String eachUrl : urls) {
+            driver.get(eachUrl);
+            titles.add(driver.getTitle());
+            driver.getCurrentUrl();
+            currentURLs.add(driver.getCurrentUrl());
+        }
+
+        System.out.println(titles);
+
+
+        if (titles.size()==1){
+            System.out.println("All have the same title");
+        }else{
+            System.out.println("All have not same title");
+        }
+
+        //=======Verify that all URLs of all pages start with http://practice.cybertekschool.com ===============
+        System.out.println(currentURLs);
+
+        for (String eachURL : currentURLs) {
+
+            if (eachURL.startsWith("http://practice.cybertekschool.com")){
+                continue;
+            }else{
+                System.out.println("All pages does NOT start with http://practice.cybertekschool.com");
+            }
+        }
+
+
+        driver.quit();
+
+
+    }
+}
