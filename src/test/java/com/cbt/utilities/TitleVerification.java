81f29903cc51da17619034d064cb227e9ed0f12d
tekemehmet <tekemehmet@yahoo.com>
2020-03-07 10:33:43 -0500

    Update-2

--- src/test/java/com/cbt/utilities/TitleVerification.java
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -1,9 +1,6 @@
 package com.cbt.utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.*;
 
 public class TitleVerification {
--- src/test/java/com/cbt/utilities/TitleVerification3.java
+++ src/test/java/com/cbt/utilities/TitleVerification3.java
@@ -8,17 +8,27 @@ import java.util.List;
 
 public class TitleVerification3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        List<String> browserList = new ArrayList<>(Arrays.asList("chrome", "edge" , "firefox","safari"));
+        List<String> browserList = new ArrayList<>(Arrays.asList("chrome", "edge" , "firefox"));
 
         for (String eachBrowser : browserList ) {
 
             WebDriver driver = BrowserFactory.getDriver(eachBrowser);
+            TitleVerification.TitleUnique(eachBrowser);
 
+            driver.quit();
+        }
+        for (String eachBrowser : browserList) {
 
+            WebDriver driver = BrowserFactory.getDriver(eachBrowser);
+            TitleVerification2.TitleVerify(eachBrowser);
+
+            driver.quit();
+
+        }
 
 
         }
     }
-}
+
